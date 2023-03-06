440daf3f725b68b8ca83f2c66b325454552bc3da
Roni <ronivon.costa@gmail.com>
2023-03-06 21:45:22 +0000

    v1.4 with optimized write speed, and optional super slow /r for problematic silicon

--- software/build.sh
+++ software/build.sh
@@ -1,4 +1,4 @@
 ~/Dev/bin/z80asm --output=$1.com $1.com.asm
-cp at28c256.com /Users/ronivon/Dev/github//C_Dev/MSX_Fusion-C_V1.2/WorkingFolder/Tools/MSX-DOS/MSXDOS-103/
+cp at28c256.com ~/Dev/github//C_Dev/MSX_Fusion-C_V1.2/WorkingFolder/Tools/MSX-DOS/MSXDOS-103/
 
 
